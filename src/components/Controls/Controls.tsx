ca49e7535dd53ab08b39de21d9ebefbfacd238dc
dayvidwhy <dayvidwhy@gmail.com>
2024-04-09 19:55:49 +1000

    Added more controls for canvas filters.

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,8 +1,9 @@
-import { component$, $, useContext } from"@builder.io/qwik";
+import { component$, $, useContext, useSignal } from"@builder.io/qwik";
 import { StoreContext } from "~/utils/store";
 
 export const Controls = component$(() => {
-    const { strokeColor, strokeSize, canvasRef } = useContext(StoreContext);
+    const { strokeColor, strokeSize, canvasRef, imageSrc } = useContext(StoreContext);
+    const copyCanvasRef = useSignal<HTMLCanvasElement>();
 
     // download the image
     const downloadImage = $(() => {
@@ -17,6 +18,37 @@ export const Controls = component$(() => {
             a.click();
             URL.revokeObjectURL(url);
         }));
+    });
+
+    const resetImage = $(() => {
+        const img = new Image();
+        img.onload = () => {
+            const ctx = canvasRef.value.getContext("2d");
+            ctx?.drawImage(img, 0, 0, canvasRef.value.width, canvasRef.value.height);
+        };
+        img.src = imageSrc.value;
+    });
+
+    const applyEffect = $((effect: string) => {
+        const ctx = canvasRef.value.getContext("2d");
+        if (!ctx || !copyCanvasRef.value) return;
+        if (imageSrc.value === "") return;
+
+        // Set the hiddne canvas size to the current image size
+        copyCanvasRef.value.width = canvasRef.value.width;
+        copyCanvasRef.value.height = canvasRef.value.height;
+        
+        // get the hidden canvas context
+        const copyCtx = copyCanvasRef.value.getContext("2d");
+        if (!copyCtx) return;
+
+        // Copy the canvas, set the effect, then draw it back on
+        copyCtx.drawImage(canvasRef.value, 0, 0);
+        ctx.filter = effect;
+        ctx.drawImage(copyCanvasRef.value, 0, 0);
+
+        // turn off the filter for further drawing
+        ctx.filter = "none";
     });
 
     return (
@@ -54,12 +86,78 @@ export const Controls = component$(() => {
                         strokeColor.value = target.value;
                     })} />
             </div>
-            <button
-                preventdefault:click
-                onClick$={downloadImage}
-                class="w-full mt-2 px-2 text-md border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
-                    Download
-            </button>
+            <div class="flex flex-row w-full mt-2 justify-between">
+                <button
+                    preventdefault:click
+                    onClick$={resetImage}
+                    class="w-1/2 mt-1 px-2 mr-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Reset
+                </button>
+                <button
+                    preventdefault:click
+                    onClick$={downloadImage}
+                    class="w-1/2 mt-1 px-2 ml-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Download
+                </button>
+            </div>
+            <div class="flex flex-row w-full mt-2 justify-between">
+                <button
+                    preventdefault:click
+                    onClick$={$(() => {
+                        applyEffect("grayscale(100%)");
+                    })}
+                    class="w-1/2 mt-1 px-2 mr-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Grayscale
+                </button>
+                <button
+                    preventdefault:click
+                    onClick$={$(() => {
+                        applyEffect("blur(4px)");
+                    })}
+                    class="w-1/2 mt-1 px-2 ml-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Blur
+                </button>
+            </div>
+            <div class="flex flex-row w-full mt-2 justify-between">
+                <button
+                    preventdefault:click
+                    onClick$={$(() => {
+                        applyEffect("invert(100%)");
+                    })}
+                    class="w-1/2 mt-1 px-2 mr-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Invert
+                </button>
+                <button
+                    preventdefault:click
+                    onClick$={$(() => {
+                        applyEffect("contrast(200%)");
+                    })}
+                    class="w-1/2 mt-1 px-2 ml-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Contrast
+                </button>
+            </div>
+            <div class="flex flex-row w-full mt-2 justify-between border-b border-slate-400 pb-2">
+                <button
+                    preventdefault:click
+                    onClick$={$(() => {
+                        applyEffect("brightness(0.4)");
+                    })}
+                    class="w-1/2 mt-1 px-2 mr-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Darken
+                </button>
+                <button
+                    preventdefault:click
+                    onClick$={$(() => {
+                        applyEffect("brightness(1.6)");
+                    })}
+                    class="w-1/2 mt-1 px-2 ml-1 text-sm border rounded border-slate-400 hover:bg-slate-600 hover:text-slate-50">
+                        Brighten
+                </button>
+            </div>
+            <canvas
+                ref={copyCanvasRef}
+                class="hidden"
+            />
         </div>
     );
 });
